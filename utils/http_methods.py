b2d2de42b31c747a7a79382d02d7e63f664c9ae9
Evg1971 <mosoloeve@gmail.com>
2025-11-18 23:35:10 +0300

    Добавление файла

---
+++ utils/http_methods.py
@@ -0,0 +1,38 @@
+# Импортируем библиотеку requests для выполнения HTTP-запросов
+import requests
+
+# Класс HttpMethods предоставляет статические методы для выполнения различных HTTP-запросов
+class HttpMethods:
+    # Заголовки по умолчанию для всех запросов (указываем, что данные передаются в формате JSON)
+    headers = {'Content-type': 'application/json'}
+
+    # Куки, которые могут использоваться в запросах (изначально пустые)
+    cookie = ''
+
+    # Статический метод для выполнения GET-запроса
+    @staticmethod
+    def get(url):
+        # Выполняем GET-запрос по указанному URL с заголовками и куками
+        result = requests.get(url, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+        return result
+
+    # Статический метод для выполнения POST-запроса
+    @staticmethod
+    def post(url, body):
+        # Выполняем POST-запрос по указанному URL с телом запроса в формате JSON, заголовками и куками
+        result = requests.post(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+        return result
+
+    # Статический метод для выполнения PUT-запроса
+    @staticmethod
+    def put(url, body):
+        # Выполняем PUT-запрос по указанному URL с телом запроса в формате JSON, заголовками и куками
+        result = requests.put(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+        return result
+
+    # Статический метод для выполнения DELETE-запроса
+    @staticmethod
+    def delete(url, body):
+        # Выполняем DELETE-запрос по указанному URL с телом запроса в формате JSON, заголовками и куками
+        result = requests.delete(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+        return result
