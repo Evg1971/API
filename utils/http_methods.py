f48332b6080f0aeac296a41e62a728c7208c19a9
Evg1971 <mosoloeve@gmail.com>
2025-11-19 08:05:49 +0300

    ДЗ: Построение структуры проекта. Создание кастомных методов.

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,6 +1,7 @@
 # Импортируем библиотеку requests для выполнения HTTP-запросов
 import requests
 
+
 # Класс HttpMethods предоставляет статические методы для выполнения различных HTTP-запросов
 class HttpMethods:
     # Заголовки по умолчанию для всех запросов (указываем, что данные передаются в формате JSON)
@@ -8,32 +9,25 @@ class HttpMethods:
     # Куки, которые могут использоваться в запросах (изначально пустые)
     cookie = ''
 
-    # Статический метод для выполнения GET-запроса
-    @staticmethod
+    @staticmethod  # Статический метод для выполнения GET-запроса
     def get(url):
         # Выполняем GET-запрос по указанному URL с заголовками и куками
         result = requests.get(url, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
         return result
 
-
-    # Статический метод для выполнения POST-запроса
-    @staticmethod
+    @staticmethod  # Статический метод для выполнения POST-запроса
     def post(url, body):
         # Выполняем POST-запрос по указанному URL с телом запроса в формате JSON, заголовками и куками
         result = requests.post(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
         return result
 
-
-    # Статический метод для выполнения PUT-запроса
-    @staticmethod
+    @staticmethod  # Статический метод для выполнения PUT-запроса
     def put(url, body):
         # Выполняем PUT-запрос по указанному URL с телом запроса в формате JSON, заголовками и куками
         result = requests.put(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
         return result
 
-
-    # Статический метод для выполнения DELETE-запроса
-    @staticmethod
+    @staticmethod  # Статический метод для выполнения DELETE-запроса
     def delete(url, body):
         # Выполняем DELETE-запрос по указанному URL с телом запроса в формате JSON, заголовками и куками
         result = requests.delete(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
