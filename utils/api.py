d04fcedfac5a42a3f1895064d6da65f44e6fc108
Evg1971 <mosoloeve@gmail.com>
2025-12-08 22:27:20 +0300

    ДЗ: Метод Put.

--- utils/api.py
+++ utils/api.py
@@ -1,43 +1,48 @@
-from utils.http_methods import HttpMethods  # Импортируем класс HttpMethods для выполнения HTTP-запросов.
+# Импортируем класс HttpMethods из модуля utils.http_methods для выполнения HTTP-запросов.
+from utils.http_methods import HttpMethods
 
 
 class GoogleMapsApi:
-    # Базовый URL API, к которому будут отправляться запросы.
+    # Базовый URL API, к которому будут отправляться все запросы.
     base_url = 'https://rahulshettyacademy.com'
-    # Ключ для доступа к API, добавляется к запросам в качестве параметра.
+
+    # Ключ API, который добавляется к каждому запросу в качестве параметра.
     key = '?qaclick123'
 
     @staticmethod
     def create_new_place():
-        # JSON-объект с данными, необходимыми для создания нового места.
-        # Содержит информацию о местоположении, точности, названии, телефоне, адресе и других атрибутах.
+        """
+        Метод для создания нового места через API.
+        Возвращает результат POST-запроса.
+        """
+        # JSON-объект с данными для создания нового места.
         json_for_create_new_place = {
             "location": {
-                "lat": -38.383494,  # Широта местоположения.
-                "lng": 33.427362    # Долгота местоположения.
+                "lat": -38.383494,  # Координата широты местоположения.
+                "lng": 33.427362   # Координата долготы местоположения.
             },
-            "accuracy": 50,         # Точность координат.
-            "name": "Frontline house",  # Название места.
-            "phone_number": "(+91) 983 893 3937",  # Контактный телефон.
+            "accuracy": 50,  # Точность координат местоположения.
+            "name": "Frontline house",  # Название создаваемого места.
+            "phone_number": "(+91) 983 893 3937",  # Контактный телефон места.
             "address": "29, side layout, cohen 09",  # Адрес места.
             "types": [
                 "shoe park",  # Тип места.
                 "shop"        # Тип места.
             ],
             "website": "http://google.com",  # Веб-сайт места.
-            "language": "French-IN"  # Язык, на котором представлена информация.
+            "language": "French-IN"  # Язык информации о месте.
         }
 
-        # Формируем ресурсный путь для POST-запроса.
+        # Ресурсный путь для POST-запроса на создание нового места.
         post_resource = '/maps/api/place/add/json'
 
-        # Формируем полный URL для POST-запроса, объединяя базовый URL, ресурсный путь и ключ.
+        # Формируем полный URL для POST-запроса.
         post_url = GoogleMapsApi.base_url + post_resource + GoogleMapsApi.key
 
         # Выводим сформированный URL для отладки.
         print(post_url)
 
-        # Выполняем POST-запрос для создания нового места, передавая сформированный URL и JSON-данные.
+        # Выполняем POST-запрос для создания нового места.
         result_post = HttpMethods.post(post_url, json_for_create_new_place)
 
         # Выводим текст ответа от сервера для отладки.
@@ -48,7 +53,9 @@ class GoogleMapsApi:
 
     @staticmethod
     def get_new_place(place_id):
-        # Формируем ресурсный путь для GET-запроса.
+        """ Метод для получения информации о месте по его идентификатору.
+        Возвращает результат GET-запроса."""
+        # Ресурсный путь для GET-запроса.
         get_resource = '/maps/api/place/get/json'
 
         # Формируем полный URL для GET-запроса, добавляя place_id в качестве параметра.
@@ -57,7 +64,7 @@ class GoogleMapsApi:
         # Выводим сформированный URL для отладки.
         print(get_url)
 
-        # Выполняем GET-запрос для получения информации о месте по его place_id.
+        # Выполняем GET-запрос для получения информации о месте.
         result_get = HttpMethods.get(get_url)
 
         # Выводим текст ответа от сервера для отладки.
@@ -65,3 +72,32 @@ class GoogleMapsApi:
 
         # Возвращаем результат GET-запроса.
         return result_get
+
+    @staticmethod
+    def put_new_place(place_id):
+        """ Метод для обновления информации о месте по его идентификатору.
+        Возвращает результат PUT-запроса. """
+        # Ресурсный путь для PUT-запроса.
+        put_resource = '/maps/api/place/update/json'
+
+        # Формируем полный URL для PUT-запроса.
+        put_url = GoogleMapsApi.base_url + put_resource + GoogleMapsApi.key
+
+        # Выводим сформированный URL для отладки.
+        print(put_url)
+
+        # JSON-объект с данными для обновления информации о месте.
+        json_for_update_new_location = {
+            "place_id": place_id,  # Идентификатор места.
+            "address": "100 Lenina street, RU",  # Новый адрес места.
+            "key": "qaclick123"  # Ключ API.
+        }
+
+        # Выполняем PUT-запрос для обновления информации о месте.
+        result_put = HttpMethods.put(put_url, json_for_update_new_location)
+
+        # Выводим текст ответа от сервера для отладки.
+        print(result_put.text)
+
+        # Возвращаем результат PUT-запроса.
+        return result_put
